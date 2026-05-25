ced76307dc2b20de185817fd3f682af2dcf06bc1
josephtroyapw-a11y <josephtroyapw@gmail.com>
2026-05-25 18:01:50 +0700

    Update semua file editan terbaru

---
+++ database/seeders/SuratSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Surat;
+use App\Models\Penduduk;
+
+class SuratSeeder extends Seeder
+{
+    public function run(): void{
+    // Mengambil sampel penduduk pertama dari database
+    $warga1 = Penduduk::first();
+
+    if ($warga1) {
+        Surat::create([
+            'nomor_surat' => '001/MK/2026',
+            'jenis_surat' => 'Surat Keterangan Usaha (SKU)',
+            'tanggal_ajuan' => '2026-05-15',
+            'penduduk_id' => $warga1->id
+        ]);
+
+        Surat::create([
+            'nomor_surat' => '002/MK/2026',
+            'jenis_surat' => 'Surat Pengantar SKCK',
+            'tanggal_ajuan' => '2026-05-18',
+            'penduduk_id' => $warga1->id
+        ]);
+    }
+    }
+}
+ 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuratController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/surat', [SuratController::class, 'daftarSurat']);
